a5b1677f6f9dbd1c77dbcb8b4e47414e7b386ab5
Phạm Việt Hùng <phamviethung365@gmail.com>
2024-10-29 23:59:17 +0700

    xcelium

---
+++ 02_sim/xcelium.d/run.lnx8664.23.09.d/files.ts
@@ -0,0 +1,6 @@
+1724940620 /earth/mars/mars04/workspace/School_Pj/Exercise1/00_src/register.sv
+1724768073 /earth/mars/mars04/workspace/School_Pj/Exercise1/00_src/Exercise1.sv
+1724341233 /earth/mars/mars04/workspace/School_Pj/Exercise1/00_src/adder.sv
+1724418578 /earth/mars/mars04/workspace/School_Pj/Exercise1/00_src/compare_A_lt_B.sv
+1724418124 /earth/mars/mars04/workspace/School_Pj/Exercise1/00_src/FSM.sv
+1724387187 /earth/mars/mars04/workspace/School_Pj/Exercise1/01_tb/testbench.sv
